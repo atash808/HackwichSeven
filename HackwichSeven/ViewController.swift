a265b86cd0c15d47ec5b8f14328849de83949aa3
atash808 <atash@hawaii.edu>
2022-10-18 21:14:53 -1000

    Completed Parts 1-8

--- HackwichSeven/ViewController.swift
+++ HackwichSeven/ViewController.swift
@@ -2,18 +2,31 @@
 //  ViewController.swift
 //  HackwichSeven
 //
-//  Created by Flynn Porter on 10/18/22.
+//  Created by Atash Imanverdi on 10/18/22.
 //
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField: UITextField!
+    
+    @IBOutlet var displayLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        displayLabel.text = ""
+   
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any)
+    {
+        let userInputText = textField.text
+        displayLabel.text = userInputText
+    
+    }
+    
 }
 
